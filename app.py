5a576a3413a579ee17fc573d55e7274954d421d3
Dominik <dominik.merdzik@onet.pl>
2024-05-29 16:27:34 +0200

    changed date to issue date

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class RowForm(FlaskForm):
     prep_checkbox = BooleanField('Prep')
     prep_date = DateField('Prep Date')
     assigned_to = StringField('Assigned To', validators=[DataRequired()])
-    date = DateField('Date', validators=[DataRequired()])
+    issue_date = DateField('Date', validators=[DataRequired()])
     in_scope = BooleanField('In Scope')
     comments = TextAreaField('Comments')
     rca = StringField('Root Cause Analysis')
@@ -33,16 +33,16 @@ class Row(db.Model):
     incident = db.Column(db.String)
     prep = db.Column(db.String)
     assigned_to = db.Column(db.String(100), nullable=True)
-    date = db.Column(db.String)
+    issue_date = db.Column(db.String)
     in_scope = db.Column(db.String)
     comments = db.Column(db.String)
     rca = db.Column(db.String)
 
-    def __init__(self, incident, prep, assigned_to, date, in_scope, comments, rca):
+    def __init__(self, incident, prep, assigned_to, issue_date, in_scope, comments, rca):
         self.incident = incident
         self.prep = prep
         self.assigned_to = assigned_to
-        self.date = date
+        self.issue_date = issue_date
         self.in_scope = in_scope
         self.comments = comments
         self.rca = rca
@@ -61,7 +61,7 @@ def index():
             incident=form.incident.data,
             prep=prep,
             assigned_to=form.assigned_to.data,
-            date=form.date.data,
+            issue_date=form.issue_date.data,
             in_scope=in_scope,
             comments=form.comments.data,
             rca=form.rca.data
@@ -78,7 +78,7 @@ def edit_row(row_id):
     row.incident = request.form['incident']
     row.prep = request.form['prep']
     row.assigned_to = request.form['assigned_to']
-    row.date = request.form['date']
+    row.issue_date = request.form['issue_date']
     row.in_scope = 'Yes' if request.form.get('in_scope') == 'true' else 'No'
     app.logger.debug(f"In scope checkbox: {row.in_scope}")
     row.comments = request.form['comments']
@@ -108,7 +108,7 @@ def save_changes():
             incident = edited_rows[f'incident_{row_id}']
             prep = edited_rows.get(f'prep_{row_id}', '')
             assigned_to = edited_rows.get(f'assigned_to_{row_id}', '')
-            date = edited_rows[f'date_{row_id}']
+            issue_date = edited_rows[f'issue_date_{row_id}']
             in_scope = edited_rows.get(f'in_scope_{row_id}')
             comments = edited_rows[f'comments_{row_id}']
             rca = edited_rows[f'rca_{row_id}']
@@ -116,7 +116,7 @@ def save_changes():
             row.incident = incident
             row.prep = prep
             row.assigned_to = assigned_to
-            row.date = date
+            row.issue_date = issue_date
             row.in_scope = in_scope
             row.comments = comments
             row.rca = rca
